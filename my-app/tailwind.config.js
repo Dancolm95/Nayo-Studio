72f81cb6f24ffcead4fee699761284f88d3ace03
dancolm95 <dancolm95@gmail.com>
2025-08-07 22:27:05 -0500

    add structure and tailwind config

---
+++ my-app/tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    colors: {
+      primary: '#FF6B00', // ejemplo de tu paleta
+      secondary: '#1B1B1B',
+    },
+    fontFamily: {
+      heading: ['"Poppins"', 'sans-serif'],
+      body: ['"Inter"', 'sans-serif'],
+    },
+  },
+};
