984eb2572889dd31b7c292b5d22d13d477a080ae
Daniel Colona <dancolm95@gmail.com>
2025-08-24 14:34:18 -0500

    fix bug for deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,11 @@ import { Header } from '@/components/layout/Header';
 import { Footer } from '@/components/layout/Footer';
 import './globals.css';
 
+export const metadata = {
+  title: 'Nayo-Studio',
+  description: 'Landing Page',
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -11,9 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Header />
-        <main>
-          {children}
-        </main>
+        <main>{children}</main>
         <Footer />
       </body>
     </html>
